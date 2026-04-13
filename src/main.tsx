cb42e8c7f68397d5ec09931bcea5defbd21768f8
Mikołaj Michalak <majkanart117@gmail.com>
2026-04-13 19:41:02 +0200

    feat: Convert project to TypeScript

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
+/// <reference types="vite/client" />
+
 import { StrictMode } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './App.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <App />
+      </BrowserRouter>
+    </StrictMode>,
+  )
+}
